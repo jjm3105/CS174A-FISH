f2d54e4caf7e4c3366f494601b7e35a41ed348f0
jjm3105 <jjm3105@gmail.com>
2019-03-12 06:20:16 -0700

    Update addtional shapes for fish

--- files/shapes.js
+++ files/shapes.js
@@ -8,49 +8,6 @@ window.Square = window.classes.Square = class Square extends Shape {
     }
 }
 
-//////////////////////////// Tailfin object 
-window.tailfin = window.classes.tailfin = class tailfin extends Shape {
-    constructor() {
-        super("positions", "normals", "texture_coords");
-        this.positions.push(     ...Vec.cast(
-                                            [-0.5, 1, 0.2], [-1, -1, 0.2], [0, 0, 0.2], [1, -1, 0.2], [0.5, 1, 0.2],    //face front
-                                            [-0.5, 1, -0.2], [-1, -1, -0.2], [0, 0, -0.2], [1, -1, -0.2], [0.5, 1, -0.2], //face back
-                                            [-0.5, 1, 0.2], [-0.5, 1, -0.2], [0.5, 1, -0.2], [0.5, 1, 0.2], // 1 6 10 5 bottom
-                                            [0.5, 1, 0.2], [0.5, 1, -0.2], [1, -1, 0.2], [1, -1, -0.2], //5 10 4 9 left outside
-                                            [-0.5, 1, 0.2], [-0.5, 1, -0.2], [-1, -1, -0.2], [-1, -1, 0.2], //1 6 7 2 right outside
-                                            [0, 0, 0.2], [0, 0, -0.2], [1, -1, -0.2], [1, -1, 0.2], // 3 8 9 4 left inside
-                                            [-1, -1, 0.2], [-1, -1, -0.2], [0, 0, -0.2], [0, 0, 0.2], //2 7 8 3 right inside
-                                        ));
-        this.normals.push(       ...Vec.cast(
-                                            [ 0,  0, 1], [0,  0, 1], [ 0, 0, 1], [0, 0, 1], [0, 0, 1], 
-                                            [ 0,  0, -1], [0,  0, -1], [ 0, 0, -1], [0, 0, -1], [0, 0, -1],
-                                            [ 0,  1, 0], [0,  1, 0], [ 0, 1, 0], [0, 1, 0],
-                                            [ 0.9701,  0.2426, 0], [ 0.9701,  0.2426, 0], [ 0.9701,  0.2426, 0], [ 0.9701,  0.2426, 0], 
-                                            [ -0.9701,  0.2426, 0], [ -0.9701,  0.2426, 0], [ -0.9701,  0.2426, 0], [ -0.9701,  0.2426, 0],
-                                            [ -0.707106,  -0.707106, 0], [ -0.707106,  -0.707106, 0], [ -0.707106,  -0.707106, 0], [ -0.707106,  -0.707106, 0], 
-                                            [ 0.707106,  -0.707106, 0], [ 0.707106,  -0.707106, 0], [ 0.707106,  -0.707106, 0], [ 0.707106,  -0.707106, 0] 
-                                            ));
-        this.texture_coords.push(...Vec.cast([0.25, 1],     [0, 0],     [ 0.5, 0.5],    [1, 0]   ,   [0.75, 1],
-                                             [0.25, 1],     [0, 0],     [ 0.5, 0.5],    [1, 0]   ,   [0.75, 1],
-                                             [1,1], [0,1], [1,0], [0,0], 
-                                             [1,1], [0,1], [1,0], [0,0], 
-                                             [1,1], [0,1], [1,0], [0,0],  
-                                             [1,1], [0,1], [1,0], [0,0], 
-                                             [1,1], [0,1], [1,0], [0,0] 
-                                             ));
-        this.indices.push(  0, 1, 2, 0, 4, 2, 2, 4, 3,
-                            5, 6, 7, 5, 9, 7, 7, 9, 8,
-                            0, 5, 4, 5, 9, 4,
-                            4, 9, 3, 8, 3, 9,
-                            0, 5, 1, 6, 5, 1,
-                            3, 2, 7, 8, 7, 3,
-                            1, 6, 7, 1, 2, 7
-                            );
-    }
-}
-////////////////////////////////////////////
-
-
 window.Circle = window.classes.Circle = class Circle extends Shape {
     constructor(sections) {
         super("positions", "normals", "texture_coords");
@@ -113,7 +70,6 @@ window.Cube = window.classes.Cube = class Cube extends Shape {
         );
     }
 }
-
 
 window.SimpleCube = window.classes.SimpleCube = class SimpleCube extends Shape {
     constructor() {
@@ -275,3 +231,333 @@ window.Subdivision_Sphere = window.classes.Subdivision_Sphere = class Subdivisio
         this.subdivideTriangle(ab, bc, ac, count - 1);
     }
 }
+
+
+
+
+
+
+
+    /* CHANGES MADE HERE */
+
+window.Body = window.classes.Body = class Body extends Shape {
+    constructor() {
+        let max_subdivisions = 4;
+
+        super("positions", "normals", "texture_coords");
+
+        // Start from the following equilateral tetrahedron:
+        this.positions.push(...Vec.cast([0, 0, -1], [0, .9428, .3333], [-.8165, -.4714, .3333], [.8165, -.4714, .3333]));
+
+        // Begin recursion.
+        this.subdivideTriangle(0, 1, 2, max_subdivisions);
+        this.subdivideTriangle(3, 2, 1, max_subdivisions);
+        this.subdivideTriangle(1, 0, 3, max_subdivisions);
+        this.subdivideTriangle(0, 2, 3, max_subdivisions);
+
+        for (let p of this.positions) {
+            this.normals.push(p.copy());
+            this.texture_coords.push(Vec.of(
+                0.5 + Math.atan2(p[2], p[0]) / (2 * Math.PI),
+                0.5 - Math.asin(p[1]) / Math.PI));
+        }
+
+        // Fix the UV seam by duplicating vertices with offset UV
+        let tex = this.texture_coords;
+        for (let i = 0; i < this.indices.length; i += 3) {
+            const a = this.indices[i], b = this.indices[i + 1], c = this.indices[i + 2];
+            if ([[a, b], [a, c], [b, c]].some(x => (Math.abs(tex[x[0]][0] - tex[x[1]][0]) > 0.5))
+                && [a, b, c].some(x => tex[x][0] < 0.5))
+            {
+                for (let q of [[a, i], [b, i + 1], [c, i + 2]]) {
+                    if (tex[q[0]][0] < 0.5) {
+                        this.indices[q[1]] = this.positions.length;
+                        this.positions.push(this.positions[q[0]].copy());
+                        this.normals.push(this.normals[q[0]].copy());
+                        tex.push(tex[q[0]].plus(Vec.of(1, 0)));
+                    }
+                }
+            }
+        }
+    }
+
+    subdivideTriangle(a, b, c, count) {
+        if (count <= 0) {
+            this.indices.push(a, b, c);
+            return;
+        }
+
+        let ab_vert = this.positions[a].mix(this.positions[b], 0.5).normalized(),
+            ac_vert = this.positions[a].mix(this.positions[c], 0.5).normalized(),
+            bc_vert = this.positions[b].mix(this.positions[c], 0.5).normalized();
+
+        let ab = this.positions.push(ab_vert) - 1,
+            ac = this.positions.push(ac_vert) - 1,
+            bc = this.positions.push(bc_vert) - 1;
+
+        this.subdivideTriangle( a, ab, ac, count - 1);
+        this.subdivideTriangle(ab,  b, bc, count - 1);
+        this.subdivideTriangle(ac, bc,  c, count - 1);
+        this.subdivideTriangle(ab, bc, ac, count - 1);
+    }
+}
+
+/*
+window.Fin = window.classes.Fin = class Fin extends Shape {
+    constructor() { 
+        // Name the values we'll define per each vertex.
+        super("positions", "normals", "texture_coords");
+        
+        this.positions.push(...Vec.cast(
+            [0, 0,  1], [ 0, 1,  1], [1, 0,  1],
+            [0, 0, -1], [ 0, 1, -1], [1, 0, -1],
+            [0, 0,  1], [ 0, 0, -1], [1, 0, -1], [1, 0, 1],
+            [0, 0,  1], [ 0, 0, -1], [0, 1, -1], [0, 1, 1],
+            [1, 0,  1], [ 1, 0, -1], [0, 1, -1], [0, 1, 1]));
+
+        // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+        // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+        const r2 = Math.sqrt(2);
+        this.normals.push(...Vec.cast(
+            [ 0,  0,  1], [ 0,  0,  1], [ 0,  0,  1],
+            [ 0,  0, -1], [ 0,  0, -1], [ 0,  0, -1],
+            [ 0, -1,  0], [ 0, -1,  0], [ 0, -1,  0], [ 0, -1,  0],
+            [-1,  0,  0], [-1,  0,  0], [-1,  0,  0], [-1,  0,  0],
+            [r2, r2,  0], [r2, r2,  0], [r2, r2,  0], [r2, r2,  0]));
+
+        // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+        // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+        // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+        this.indices.push(
+            0, 1, 2,
+            3, 4, 5,
+            6, 7, 8, 6, 8, 9,
+            10, 11, 12, 10, 13, 12,
+            14, 15, 16, 14, 17, 16);
+    }
+}
+ */
+
+window.End1 = window.classes.End1 = class End1 extends Shape {
+    constructor() {
+        let sections = 20;
+        let front = 0.96;
+        let back = 0.775;
+        
+        super("positions", "normals", "texture_coords");
+
+        this.positions.push(...Vec.cast([front, 0, 0.5], [back, 0, -0.5]));
+        this.normals.push(...Vec.cast(  [1, 0, 0], [1, 0,  0]));
+        this.texture_coords.push(...Vec.cast([0, 1], [0, 0]));
+
+        for (let i = 0; i < sections; ++i) {
+            const ratio = (i + 1) / sections,
+                angle = 2 * Math.PI * ratio,
+                v = Vec.of(Math.cos(angle), Math.sin(angle)),
+                id = 2 * i + 2;
+
+            this.positions.push(...Vec.cast([front*v[0], front*v[1], 0.5], [back*v[0], back*v[1], -0.5]));
+            this.normals.push(...Vec.cast(  [v[0], v[1], 0], [v[0], v[1],  0]));
+            this.texture_coords.push(...Vec.cast([ratio/2, 1], [ratio/2, 0]));
+            this.texture_coords.push(...Vec.cast([(ratio-1)/2, 1], [(ratio-1)/2, 0]));
+            this.indices.push(
+                id, id - 1, id + 1,
+                id, id - 1, id - 2);
+        }
+    }
+}
+
+window.End2 = window.classes.End2 = class End2 extends Shape {
+    constructor() {
+        let sections = 20;
+//         let front = 0.8;
+        let front = 0.79
+        let back = 0.5;
+        let factor = 0.3;
+        
+        super("positions", "normals", "texture_coords");
+
+        this.positions.push(...Vec.cast([front, 0, 0.5], [factor*back, 0, -0.5]));
+        this.normals.push(...Vec.cast(  [1, 0, 0], [1, 0,  0]));
+        this.texture_coords.push(...Vec.cast([0, 1], [0, 0]));
+
+        for (let i = 0; i < sections; ++i) {
+            const ratio = (i + 1) / sections,
+                angle = 2 * Math.PI * ratio,
+                v = Vec.of(Math.cos(angle), Math.sin(angle)),
+                v2 = Vec.of(Math.cos(angle), factor*Math.sin(angle)),
+                id = 2 * i + 2;
+
+            this.positions.push(...Vec.cast([front*v[0], front*v[1], 0.5], [back*v2[0], back*v2[1], -0.5]));
+            this.normals.push(...Vec.cast(  [v[0], v[1], 0], [v[0], v[1],  0]));
+            this.texture_coords.push(...Vec.cast([ratio/2, 1], [ratio/2, 0]));
+            this.texture_coords.push(...Vec.cast([(ratio-1)/2, 1], [(ratio-1)/2, 0]));
+            this.indices.push(
+                id, id - 1, id + 1,
+                id, id - 1, id - 2);
+        }
+    }
+}
+
+window.End3 = window.classes.End3 = class End3 extends Shape {
+    constructor() {
+        super("positions", "normals", "texture_coords");
+        
+        const b_w = 0.28;    // x
+        const b_l = 0.475;  // y
+        const b_h = -0.4;   // z
+        const f_w = 0.5;    // x
+        const f_l = 0.4;    // y
+        const f_h = 0.6;    // z
+
+        this.positions.push(...Vec.cast(
+            [-b_w,  b_l, b_h], [-b_w, -b_l, b_h], [ b_w,  b_l, b_h], [ b_w, -b_l, b_h],
+            [-f_w, -f_l, f_h], [ f_w, -f_l, f_h], [-f_w,  f_l, f_h], [ f_w,  f_l, f_h],
+            [-f_w,  f_l, f_h], [ f_w,  f_l, f_h], [-b_w,  b_l, b_h], [ b_w,  b_l, b_h],
+            [-b_w, -b_l, b_h], [ b_w, -b_l, b_h], [-f_w, -f_l, f_h], [ f_w, -f_l, f_h],
+            [-b_w, -b_l, b_h], [-f_w, -f_l, f_h], [-b_w,  b_l, b_h], [-f_w,  f_l, f_h],
+            [ b_w, -b_l, b_h], [ f_w, -f_l, f_h], [ b_w,  b_l, b_h], [ f_w,  f_l, f_h] 
+        ));
+
+        this.texture_coords.push(...Vec.cast(
+            [0,    2/3], [0.25, 2/3], [0,    1/3], [0.25, 1/3],
+            [0.5,  2/3], [0.5,  1/3], [0.75, 2/3], [0.75, 1/3],
+            [0.75, 2/3], [0.75, 1/3], [1,    2/3], [1,    1/3],
+            [0.25, 2/3], [0.25, 1/3], [0.5,  2/3], [0.5,  1/3],
+            [0.25, 2/3], [0.5,  2/3], [0.25, 1  ], [0.5,  1  ],
+            [0.25, 1/3], [0.5,  1/3], [0.25, 0  ], [0.5,  0  ],
+        )); 
+
+        this.normals.push(...Vec.cast(
+            ...Array(4).fill([ 0,  0, -1]),
+            ...Array(4).fill([ 0,  0,  1]),
+            ...Array(4).fill([ 0,  1,  0]),
+            ...Array(4).fill([ 0, -1,  0]),
+            ...Array(4).fill([-1,  0,  0]),
+            ...Array(4).fill([ 1,  0,  0])
+        ));
+
+        this.indices.push(
+            0, 2, 1, 1, 2, 3,
+            4, 5, 6, 5, 7, 6,
+            8, 9, 10, 9, 11, 10,    
+            12, 13, 14, 13, 15, 14,
+            16, 19, 18, 16, 17, 19,
+            20, 22, 21, 21, 22, 23
+        );
+    }
+}
+
+window.Tail = window.classes.Tail = class Tail extends Shape {
+    constructor() {
+        super("positions", "normals", "texture_coords");
+        const t_w = 0.35;//0.3;    // z
+        const t_l = 0.45;   // x
+        const h   = 1;        // y
+        const w   = 0.2;      // z
+        const b_w = 0.1;//0.15;   // z
+        const b_l = 1.2;    // x
+
+
+        this.positions.push(     ...Vec.cast(
+                                            [-t_l,  h,  t_w],   [-b_l, -h,  b_w],   [   0,  0,    w],   [ b_l, -h,  b_w],   [t_l, h,  t_w],  // face front
+                                            [-t_l,  h, -t_w],   [-b_l, -h, -b_w],   [   0,  0,   -w],   [ b_l, -h, -b_w],   [t_l, h, -t_w],  // face back
+                                            [-t_l,  h,  t_w],   [-t_l,  h, -t_w],   [ t_l,  h, -t_w],   [ t_l,  h,  t_w],                    // 1 6 10 5 bottom
+                                            [ t_l,  h,  t_w],   [ t_l,  h, -t_w],   [ b_l, -h,  b_w],   [ b_l, -h, -b_w],                    // 5 10 4 9 left outside
+                                            [-t_l,  h,  t_w],   [-t_l,  h, -t_w],   [-b_l, -h, -b_w],   [-b_l, -h,  b_w],                    // 1 6  7 2 right outside
+                                            [   0,  0,    w],   [   0,  0,   -w],   [ b_l, -h, -b_w],   [ b_l, -h,  b_w],                    // 3 8  9 4 left inside
+                                            [-b_l, -h,  b_w],   [-b_l, -h, -b_w],   [   0,  0,   -w],   [   0,  0,    w],                    // 2 7  8 3 right inside
+                                        ));
+        this.normals.push(       ...Vec.cast(
+                                            [ 0,  0, 1],                 [0,  0, 1],                    [ 0, 0, 1],                     [0, 0, 1],                      [0, 0, 1], 
+                                            [ 0,  0, -1],                [0,  0, -1],                   [ 0, 0, -1],                    [0, 0, -1],                     [0, 0, -1],
+                                            [ 0,  1, 0],                 [0,  1, 0],                    [ 0, 1, 0],                     [0, 1, 0],
+                                            Vec.of(2, -0.75, 0).normalized(), Vec.of(2, -0.75, 0).normalized(), Vec.of(2, -0.75, 0).normalized(), Vec.of(2, -0.75, 0).normalized(), 
+                                            Vec.of(2, 0.75, 0).normalized(),  Vec.of(2, 0.75, 0).normalized(),  Vec.of(2, 0.75, 0).normalized(),  Vec.of(2, 0.75, 0).normalized(),
+                                            Vec.of(-1, -1.2, 0).normalized(), Vec.of(-1, -1.2, 0).normalized(), Vec.of(-1, -1.2, 0).normalized(), Vec.of(-1, -1.2, 0).normalized(), 
+                                            Vec.of(1, -1.2, 0).normalized(),  Vec.of(1, -1.2, 0).normalized(),  Vec.of(1, -1.2, 0).normalized(),  Vec.of(1, -1.2, 0).normalized(),
+                                            ));
+        this.texture_coords.push(...Vec.cast([0.25, 1],     [0, 0],     [ 0.5, 0.5],    [1, 0],   [0.75, 1],
+                                             [0.25, 1],     [0, 0],     [ 0.5, 0.5],    [1, 0],   [0.75, 1],
+                                             [-1,1],         [0,1],      [1,0],          [0,0], 
+                                             [-1,1],         [0,1],      [1,0],          [0,0], 
+                                             [-1,1],         [0,1],      [1,0],          [0,0],  
+                                             [-1,1],         [0,1],      [1,0],          [0,0], 
+                                             [-1,1],         [0,1],      [1,0],          [0,0] 
+                                             ));
+        this.indices.push(  0, 1, 2, 0, 4, 2, 2, 4, 3,
+                            5, 6, 7, 5, 9, 7, 7, 9, 8,
+                            0, 5, 4, 5, 9, 4,
+                            4, 9, 3, 8, 3, 9,
+                            0, 5, 1, 6, 5, 1,
+                            3, 2, 7, 8, 7, 3,
+                            1, 6, 7, 1, 2, 7
+                            );
+    }
+}
+
+window.Eye = window.classes.Eye = class Eye extends Shape {
+    constructor() {
+        let max_subdivisions = 4;
+        
+        super("positions", "normals", "texture_coords");
+
+        // Start from the following equilateral tetrahedron:
+        this.positions.push(...Vec.cast([0, 0, -1], [0, .9428, .3333], [-.8165, -.4714, .3333], [.8165, -.4714, .3333]));
+
+        // Begin recursion.
+        this.subdivideTriangle(0, 1, 2, max_subdivisions);
+        this.subdivideTriangle(3, 2, 1, max_subdivisions);
+        this.subdivideTriangle(1, 0, 3, max_subdivisions);
+        this.subdivideTriangle(0, 2, 3, max_subdivisions);
+
+        for (let p of this.positions) {
+            this.normals.push(p.copy());
+            this.texture_coords.push(Vec.of(
+                0.5 + Math.atan2(p[2], p[0]) / (2 * Math.PI),
+                0.5 - Math.asin(p[1]) / Math.PI));
+        }
+
+        // Fix the UV seam by duplicating vertices with offset UV
+        let tex = this.texture_coords;
+        for (let i = 0; i < this.indices.length; i += 3) {
+            const a = this.indices[i], b = this.indices[i + 1], c = this.indices[i + 2];
+            if ([[a, b], [a, c], [b, c]].some(x => (Math.abs(tex[x[0]][0] - tex[x[1]][0]) > 0.5))
+                && [a, b, c].some(x => tex[x][0] < 0.5))
+            {
+                for (let q of [[a, i], [b, i + 1], [c, i + 2]]) {
+                    if (tex[q[0]][0] < 0.5) {
+                        this.indices[q[1]] = this.positions.length;
+                        this.positions.push(this.positions[q[0]].copy());
+                        this.normals.push(this.normals[q[0]].copy());
+                        tex.push(tex[q[0]].plus(Vec.of(1, 0)));
+                    }
+                }
+            }
+        }
+    }
+
+    subdivideTriangle(a, b, c, count) {
+        if (count <= 0) {
+            this.indices.push(a, b, c);
+            return;
+        }
+
+        let ab_vert = this.positions[a].mix(this.positions[b], 0.5).normalized(),
+            ac_vert = this.positions[a].mix(this.positions[c], 0.5).normalized(),
+            bc_vert = this.positions[b].mix(this.positions[c], 0.5).normalized();
+
+        let ab = this.positions.push(ab_vert) - 1,
+            ac = this.positions.push(ac_vert) - 1,
+            bc = this.positions.push(bc_vert) - 1;
+
+        this.subdivideTriangle( a, ab, ac, count - 1);
+        this.subdivideTriangle(ab,  b, bc, count - 1);
+        this.subdivideTriangle(ac, bc,  c, count - 1);
+        this.subdivideTriangle(ab, bc, ac, count - 1);
+    }
+}
+
+
+
+
